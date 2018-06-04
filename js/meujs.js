72be9be478a1a6db1eca38cfc573fddfd64b2e26
rusleysantos <rusley_silva@hotmail.com>
2018-06-04 17:43:39 -0300

    Add files via upload

---
+++ js/meujs.js
@@ -0,0 +1,31 @@
+var min = Math.ceil(0);
+var max = Math.floor(3);
+var Aleatorio = Math.floor(Math.random() * (max - min)) + min;
+var dic = ["futebol", "casa","dia"]
+var forca = dic[Aleatorio]
+var count = 0;
+$(document).ready(function () {
+    for (var i = 0; i < forca.length; i++) {
+        $('.campo').append('<input class="forca" type="text" data-forca="" data-posicao="' + i + '">');
+    }
+
+    $('.forca').each(function (i, elem) {
+        $(elem).attr('data-forca', forca[$(elem).attr('data-posicao')])
+    })
+
+});
+
+$('.submit').click(function () {
+    if (count > forca.length - 1) {
+        alert('Você perdeu!!!')
+       
+    } else {
+        if 
+            ($('[data-forca="' + $('.palava').val().toLowerCase() + '"]').length > 0)
+            $('[data-forca="' + $('.palava').val().toLowerCase() + '"]').val($('.palava').val())
+         else 
+            count++;
+
+    }
+
+})
